96c96b0040728f83d3253e246badaac27621fb46
zrr985 <158461552+zrr985@users.noreply.github.com>
2025-08-04 14:48:09 +0800

    Delete build directory

--- build/CMakeFiles/gcd_demo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for gcd_demo.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/gcd_demo.dir/src/gcd.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/gcd_demo.dir/src/gcd.cpp.o: /home/zrr/桌面/zrr/src/gcd.cpp \
- /usr/include/stdc-predef.h /home/zrr/桌面/zrr/include/gcd.h
